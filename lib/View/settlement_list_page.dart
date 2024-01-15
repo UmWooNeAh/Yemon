d991244d193697e0aa9dc0da93268eab7d4c7e24
akuby21 <39649594+akuby21@users.noreply.github.com>
2024-01-16 04:11:54 +0900

    Feat : SettlementListPage
    정산 정보 리스트 페이지(메인 화면)

--- lib/View/settlement_list_page.dart
+++ lib/View/settlement_list_page.dart
@@ -1,7 +1,35 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+
+import '../theme.dart';
+
+int size = 10;
+
+class EditManagement extends ChangeNotifier{
+  bool isEdit = false;
+  List<bool> isSelected = List.generate(size, (index) => false);
+
+  void toggleEdit(){
+    isEdit = !isEdit;
+    isSelected = List.generate(size, (index) => false);
+    notifyListeners();
+  }
+
+  void toggleSelect(int index){
+    isSelected[index] = !isSelected[index];
+    notifyListeners();
+  }
+
+  bool checkMultipleSelect(){
+    int cnt = isSelected.where((element) => element == true).length;
+
+    return cnt >= 2;
+  }
+
+}
+
+final editManagementProvider = ChangeNotifierProvider((ref) => EditManagement());
 
 class SettlementListPage extends ConsumerStatefulWidget {
   const SettlementListPage({Key? key}) : super(key: key);
@@ -11,20 +39,251 @@ class SettlementListPage extends ConsumerStatefulWidget {
 }
 
 class _SettlementListPageState extends ConsumerState<SettlementListPage> {
+
   @override
   Widget build(BuildContext context) {
+    final editManagement = ref.watch(editManagementProvider);
+    final sizes = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            OutlinedButton(onPressed: (){
-              context.go("/SettlementManagementPage");
-            }, child: const Text("엄"))
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Row(
+          children: [
+            const Text(
+              "Yemon",
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w500,
+                fontSize: 28,
+              ),
+            ),
+            Container(
+              child: FlutterLogo(),
+              margin: const EdgeInsets.only(left: 5),
+            ),
           ],
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(left: 20),
+                  child: const Text("최근 정산 목록",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap:(){
+                    editManagement.toggleEdit();
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.only(right: 20),
+                    child: Text("목록 편집",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: basic[2],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: List.generate(size, (index) => SingleSettlement(index:index))
+                ),
+              ),
+            ),
+            editManagement.isEdit && editManagement.isSelected.contains(true) ? Container(
+              color: basic[1],
+              width: sizes.width, height: 80,
+              child: GestureDetector(
+                onTap:(){},
+                child: editManagement.checkMultipleSelect() ? Container(
+                  margin: const EdgeInsets.only(left: 40, right: 40, top: 10, bottom:10),
+                  color: basic[2],
+                  child: const Center(
+                      child: Text("삭제",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700,
+                        ),
+                  ),
+                ),
+                ) :
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    GestureDetector(
+                      onTap: (){
+                        showDialog(context: context, builder: (context){
+                          return AlertDialog(
+                            title: const Text("정산 이름 변경"),
+                            content: TextField(
+                              onChanged: (value){
+
+                              },
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: (){
+                                  Navigator.pop(context);
+                                },
+                                child: const Text("취소"),
+                              ),
+                              TextButton(
+                                onPressed: (){
+                                  Navigator.pop(context);
+                                },
+                                child: const Text("확인"),
+                              ),
+                            ],
+                          );
+                        });
+                      },
+                      child: Container(
+                        width: sizes.width * 0.4,
+                        height: 60,
+                        color: basic[2],
+                        child: const Center(
+                          child: Text("이름 변경",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: (){},
+                      child: Container(
+                        width: sizes.width * 0.4,
+                        height: 60,
+                        color: basic[2],
+                        child: const Center(
+                          child: Text("삭제",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              )
+            ) :
+            const SizedBox.shrink(),
+          ]
+        ),
+      ),
+      floatingActionButton: Container(
+        margin: EdgeInsets.only(bottom: editManagement.isEdit && editManagement.isSelected.contains(true) ? 80 : 0),
+        child: FloatingActionButton(
+          onPressed: (){
+            context.push('/SettlementManagementPage');
+          },
+          backgroundColor: basic[0],
+          child: const Icon(Icons.add),
         ),
       ),
     );
   }
 }
+
+class SingleSettlement extends ConsumerStatefulWidget {
+  final int index;
+  const SingleSettlement({Key? key, required this.index}) : super(key: key);
+
+  @override
+  ConsumerState<SingleSettlement> createState() => _SingleSettlementState();
+}
+
+class _SingleSettlementState extends ConsumerState<SingleSettlement> {
+  @override
+  Widget build(BuildContext context) {
+    final editManagement = ref.watch(editManagementProvider);
+    final size = MediaQuery.of(context).size;
+    return Stack(
+      children: [
+        editManagement.isEdit ?
+        Positioned(
+          top: 35, left: 5,
+          child: Transform.scale(
+            scale: 1.6,
+            child: Checkbox(
+              value: editManagement.isSelected[widget.index],
+              onChanged: (value){
+                editManagement.toggleSelect(widget.index);
+              },
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              fillColor: MaterialStateProperty.all(basic[1]),
+              checkColor: Colors.white,
+              side: BorderSide(color: basic[1]),
+            ),
+          ),
+        ) :
+        const SizedBox.shrink(),
+        AnimatedContainer(
+          duration: const Duration(milliseconds: 300),
+          width: size.width * 0.9,
+          height: 100,
+          margin: EdgeInsets.only(left: editManagement.isEdit ? 60 : 20, right: 20, top:10, bottom: 10),
+          color: basic[1],
+          child: Container(
+            margin: const EdgeInsets.only(left: 20, right: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 15),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("정산 이름",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text("2024-01-01",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 10),
+                  child: const Text("참여자(류지원, 신성민 등 2명)",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
