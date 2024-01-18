3a8393760b97ef3956b6b1dda99771e93e9393e1
akuby21 <39649594+akuby21@users.noreply.github.com>
2024-01-19 03:32:16 +0900

    Feat : SettlementMatchingPage
    정산 매칭 기본 골자 구현

--- lib/View/settlement_matching.dart
+++ lib/View/settlement_matching.dart
@@ -1,6 +1,36 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../theme.dart';
+
+class SettlementMatchingViewmodel extends ChangeNotifier{
+  int presentReceiptIndex = 0;
+  List<bool> selectedReceiptItemIndexList = [];
+  List<bool> selectedMemberIndexList = [];
+
+  void settingMemberIndexList(int length){
+    selectedMemberIndexList = List.generate(length, (index) => false);
+    notifyListeners();
+  }
+
+  void selectReceipt(int index,int receiptItemLength){
+    presentReceiptIndex = index;
+    selectedMemberIndexList = List.generate(selectedMemberIndexList.length, (index) => false);
+    selectedReceiptItemIndexList = List.generate(receiptItemLength, (index) => false);
+    notifyListeners();
+  }
+
+  void selectReceiptItem(int index){
+    selectedReceiptItemIndexList[index] = !selectedReceiptItemIndexList[index];
+    notifyListeners();
+  }
+
+  void selectMember(int index){
+    selectedMemberIndexList.add(index);
+    notifyListeners();
+  }
+}
+
 class SettlementMatching extends ConsumerStatefulWidget {
   const SettlementMatching({super.key});
 
@@ -11,6 +41,52 @@ class SettlementMatching extends ConsumerStatefulWidget {
 class _SettlementMatchingState extends ConsumerState<SettlementMatching> {
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Settlement Matching"));
+    return Stack(
+      children: [
+        Container(
+          margin: const EdgeInsets.only(top: 15),
+          decoration: BoxDecoration(
+            color: basic[6],
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+        ReceiptCirculator(),
+        MenuSheet(),
+        GroupMembers(),
+      ],
+    );
   }
 }
+
+
+class MenuSheet extends ConsumerWidget {
+  const MenuSheet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context,WidgetRef ref) {
+    return const Placeholder();
+  }
+}
+
+class GroupMembers extends ConsumerWidget {
+  const GroupMembers({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context,WidgetRef ref) {
+    return const Placeholder();
+  }
+}
+
+class ReceiptCirculator extends ConsumerWidget {
+  const ReceiptCirculator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context,WidgetRef ref) {
+    return const Placeholder();
+  }
+}
+
+
+
+
+
