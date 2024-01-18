adb896cf0b1ebf0c258f50df276e813c4aef2603
gunwoo99 <62701141+gunwoo99@users.noreply.github.com>
2024-01-18 23:19:59 +0900

    Test Code삭제

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:sqlite_test/DB/sqlFliteDB.dart';
 
 import 'View/load_member_page.dart';
 import 'View/settlement_list_page.dart';
@@ -51,71 +50,3 @@ final GoRouter router = GoRouter(
     ),
   ],
 );
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final SqlFliteDB _model = SqlFliteDB();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            OutlinedButton(
-              onPressed: () {
-                var db = _model.database;
-              },
-              child: Text('DB 생성'),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                OutlinedButton(
-                  onPressed: () async {
-                    //await _model.insertStm("데모 정산");
-                    await _model.insertRcp(
-                        "영수증 2", "b9277cbb-e29f-4d32-bae7-0e196030590e");
-                  },
-                  child: Text('INSERT'),
-                ),
-                OutlinedButton(
-                  onPressed: () async {
-                    await _model.updateStm(
-                        "데모 정산 1", "26262c21-596a-4210-8714-3cf094a0676a");
-                  },
-                  child: Text('UPDATE'),
-                ),
-                OutlinedButton(
-                  onPressed: () async {
-                    await _model
-                        .deleteStm("26262c21-596a-4210-8714-3cf094a0676a");
-                  },
-                  child: Text('DELETE'),
-                ),
-                OutlinedButton(
-                  onPressed: () async {
-                    await _model
-                        .joinTest("b9277cbb-e29f-4d32-bae7-0e196030590e");
-                  },
-                  child: Text('Query'),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
