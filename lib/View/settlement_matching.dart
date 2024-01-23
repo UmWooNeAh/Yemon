61befffea694b0d2f4683e4eb34ee15358b37cce
akuby21 <39649594+akuby21@users.noreply.github.com>
2024-01-24 00:57:42 +0900

    Feat : Settlement Matching
    정산 매칭 구현. 취소 기능 구현예정

--- lib/View/settlement_matching.dart
+++ lib/View/settlement_matching.dart
@@ -5,6 +5,7 @@ import 'package:sqlite_test/View/menu_sheet.dart';
 import 'package:sqlite_test/View/receipt_circulator.dart';
 
 import '../Model/ReceiptItem.dart';
+import '../Model/SettlementPaper.dart';
 import '../ViewModel/mainviewmodel.dart';
 import '../theme.dart';
 import 'bottom_sheet.dart';
@@ -15,10 +16,16 @@ class SettlementMatchingViewmodel extends ChangeNotifier{
   ScrollController receiptScrollController = ScrollController();
   int presentReceiptIndex = 0;
   List<bool> selectedReceiptItemIndexList = [];
-  List<bool> selectedMemberIndexList = [];
+  List<bool> selectedMemberIndexList = [false,];
+  bool showMemberDetail = false;
 
   void settingMemberIndexList(int length){
     selectedMemberIndexList = List.generate(length, (index) => false);
+    notifyListeners();
+  }
+
+  void settingReceiptItemIndexList(int length){
+    selectedReceiptItemIndexList = List.generate(length, (index) => false);
     notifyListeners();
   }
 
@@ -38,6 +45,21 @@ class SettlementMatchingViewmodel extends ChangeNotifier{
     selectedMemberIndexList[index] = !selectedMemberIndexList[index];
     notifyListeners();
   }
+
+  void changeAllMember(bool value){
+    selectedMemberIndexList = List.generate(selectedMemberIndexList.length, (index) => value);
+    notifyListeners();
+  }
+
+
+  void toggleMemberDetail(){
+    showMemberDetail = !showMemberDetail;
+    notifyListeners();
+  }
+
+  List<int> matching(){
+     return selectedMemberIndexList.asMap().keys.where((index) => selectedMemberIndexList[index] == true).toList();
+  }
 }
 
 class SettlementMatching extends ConsumerStatefulWidget {
@@ -48,6 +70,7 @@ class SettlementMatching extends ConsumerStatefulWidget {
 }
 
 class _SettlementMatchingState extends ConsumerState<SettlementMatching> {
+
   @override
   Widget build(BuildContext context) {
     final size =  MediaQuery.of(context).size;
@@ -75,13 +98,148 @@ class GroupMembers extends ConsumerWidget {
   @override
   Widget build(BuildContext context,WidgetRef ref) {
     final size = MediaQuery.of(context).size;
-    return Container(
-      width: size.width,
-      height: 100,
-      color: basic[2]
+    final provider = ref.watch(mainProvider);
+    final sProvider = ref.watch(settlementMatchingProvider);
+    return Stack(
+      children: [
+        AnimatedContainer(
+          duration: const Duration(milliseconds: 300),
+          width: size.width,
+          height: sProvider.showMemberDetail ? 250 : 100,
+          decoration: BoxDecoration(
+            color: basic[1],
+            borderRadius: BorderRadius.only(topLeft:Radius.circular(20),topRight:Radius.circular(20)),
+
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  blurRadius: 1,
+                  spreadRadius: 3
+              ),
+            ],
+          ),
+          child:
+          SingleChildScrollView(
+            physics: const NeverScrollableScrollPhysics(),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    const Text("정산에 참여하는 사람",
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    SizedBox(width: size.width*0.2),
+                    OutlinedButton(
+                      onPressed: () {
+                        sProvider.changeAllMember(!sProvider.selectedMemberIndexList.contains(true));
+                      },
+                      style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        backgroundColor: Colors.white,
+                      ),
+                      child: Center(
+                        child: Text(sProvider.selectedMemberIndexList.contains(true) ? "선택 취소" : "전체 선택",
+                          style: TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.w600,
+                            color: Color(0xFF848484),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  width: size.width*0.95,
+                  child: sProvider.showMemberDetail ? Wrap(
+                    runSpacing: 10,
+                    crossAxisAlignment: WrapCrossAlignment.start,
+                    children: List.generate(provider.selectedSettlement.settlementPapers.length,
+                            (index) => SingleMember(index: index)),
+                  ) : SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: List.generate(provider.selectedSettlement.settlementPapers.length,
+                              (index) => SingleMember(index: index)),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ),
+        Positioned(
+          left: size.width*0.5-20,
+          child: GestureDetector(
+            onTap:(){
+              sProvider.toggleMemberDetail();
+            },
+            child:Transform.scale(
+              scale: 1.5,
+              child: sProvider.showMemberDetail ? Icon(Icons.keyboard_arrow_down) : Icon(Icons.keyboard_arrow_up),
+            )
+          ),
+        )
+      ],
     );
   }
 }
+
+class SingleMember extends ConsumerWidget {
+  final int index;
+  const SingleMember({Key? key,required this.index}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context,WidgetRef ref) {
+    final provider = ref.watch(mainProvider);
+    final sProvider = ref.watch(settlementMatchingProvider);
+    return InkWell(
+      onTap:(){
+        sProvider.selectMember(index);
+      },
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 100),
+        height: 35,
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        margin: const EdgeInsets.symmetric(horizontal: 5),
+        decoration: BoxDecoration(
+          color: sProvider.selectedMemberIndexList[index] ? basic[8] : basic[0],
+          border: Border.all(
+            color: sProvider.selectedMemberIndexList[index] ? basic[8] : basic[2],
+            width: 2,
+          ),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(0.3),
+                blurRadius: 1,
+                offset: const Offset(1.5,1.5),
+            ),
+          ],
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          children: [
+            const SizedBox(height:5),
+            Text(provider.selectedSettlement.settlementPapers[index].memberName,
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w600,
+                color: sProvider.selectedMemberIndexList[index] ? Colors.white : Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 
 
 
