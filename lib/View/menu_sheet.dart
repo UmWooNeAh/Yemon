c671ae76f9c0a8743da22e4d2ebb15055a395ac2
akuby21 <39649594+akuby21@users.noreply.github.com>
2024-01-24 00:53:47 +0900

    Feat : Menusheet
    정산 매칭의 메뉴를 보여주는 부분인 메뉴시트의 구현. 매칭까지 구현 완료 후 터치 시 확장되어 삭제되는 플로우 구현 예정

--- lib/View/menu_sheet.dart
+++ lib/View/menu_sheet.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sqlite_test/View/settlement_matching.dart';
 
 import '../Model/ReceiptItem.dart';
 import '../ViewModel/mainviewmodel.dart';
@@ -14,6 +15,7 @@ class MenuSheet extends ConsumerWidget {
   Widget build(BuildContext context,WidgetRef ref) {
     final size =  MediaQuery.of(context).size;
     final mProvider = ref.watch(mainProvider);
+    final sProvider = ref.watch(settlementMatchingProvider);
     return Container(
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height*0.6,
@@ -28,99 +30,133 @@ class MenuSheet extends ConsumerWidget {
           ),
         ],
       ),
-      child: Column(
-          children: [
-            SizedBox(height:20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text("메뉴 목록",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                SizedBox(width: size.width*0.15),
-                Container(
-                  width: size.width*0.3,
-                  height: 45,
-                  child: OutlinedButton(
-                    onPressed: () {  },
-                    style: OutlinedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      backgroundColor: basic[1],
+      child: SingleChildScrollView(
+        physics: const NeverScrollableScrollPhysics(),
+        child: Column(
+            children: [
+              SizedBox(height:20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text("메뉴 목록",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
                     ),
-                    child: Center(
-                      child: Text("전체 매칭",
-                        style: TextStyle(
-                          fontSize: 13,
-                          fontWeight: FontWeight.w600,
-                          color: Color(0xFF848484),
+                  ),
+                  SizedBox(width: size.width*0.15),
+                  Container(
+                    width: size.width*0.3,
+                    height: 45,
+                    child: OutlinedButton(
+                      onPressed: () {  },
+                      style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        backgroundColor: basic[1],
+                      ),
+                      child: Center(
+                        child: Text("전체 매칭",
+                          style: TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.w600,
+                            color: Color(0xFF848484),
+                          ),
                         ),
                       ),
+                    ),
+                  ),
+
+                ],
+              ),
+              SizedBox(height: mProvider.selectedSettlement.receipts.isEmpty ? 50 : 10),
+              mProvider.selectedSettlement.receipts.isEmpty ?
+              Text("메뉴가 없습니다.",
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w600,
+                ),
+              ) :
+              SingleChildScrollView(
+                child: Column(
+                    children : List.generate(mProvider.selectedSettlement.receipts[sProvider.presentReceiptIndex].receiptItems.length, (index) => SingleMenu(index: index))
+                ),
+              )
+            ]
+        ),
+      ),
+    );
+  }
+}
+
+class SingleMenu extends ConsumerStatefulWidget {
+  final int index;
+  const SingleMenu({super.key, required this.index});
+
+  @override
+  ConsumerState<SingleMenu> createState() => _SingleMenuState();
+}
+
+class _SingleMenuState extends ConsumerState<SingleMenu> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final sProvider = ref.watch(settlementMatchingProvider);
+    final mProvider = ref.watch(mainProvider);
+    final rcpItem = mProvider.selectedSettlement.receipts[sProvider.presentReceiptIndex].receiptItems[widget.index];
+    return Column(
+      children: [
+        InkWell(
+          onTap:(){
+            if(sProvider.selectedMemberIndexList.contains(true)){
+              //print(mProvider.selectedSettlement.settlementPapers.length);
+              List<int> userIndexes = sProvider.matching();
+              for(int index in userIndexes){
+                mProvider.matching(index, widget.index, sProvider.presentReceiptIndex);
+              }
+            }
+          },
+          child: Container(
+            height: 50,
+            color: sProvider.selectedReceiptItemIndexList[widget.index] ? basic[1] : Colors.white,
+            child: Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(left:40),
+                  width:size.width*0.25,
+                  child: Text(rcpItem.receiptItemName,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                Container(
+                  //margin: const EdgeInsets.only(right:20,left:10),
+                  width: size.width *0.25,
+                  child: Text(mProvider.selectedSettlement.receipts[sProvider.presentReceiptIndex].receiptItems[widget.index].paperOwner.length.toString()+"명",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                      color: basic[2],
+                    ),
+                  ),
+                ),
+                Container(
+                  width: size.width *0.3,
+                  child: Text(rcpItem.price.toString()+"원",
+                    textAlign: TextAlign.right,
+                    style: const TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w600,
                     ),
                   ),
                 ),
 
               ],
             ),
-            SizedBox(height:10),
-            SingleChildScrollView(
-              child: Column(
-                  // children : List.generate(mProvider.selectedSettlement.receipts.first.receiptItems.length, (index) => SingleMenu(rcpItem: mProvider.selectedSettlement.receipts.first.receiptItems[index],))
-              ),
-            )
-          ]
-      ),
-    );
-  }
-}
-
-class SingleMenu extends StatefulWidget {
-  final ReceiptItem rcpItem;
-  const SingleMenu({super.key, required this.rcpItem});
-
-  @override
-  State<SingleMenu> createState() => _SingleMenuState();
-}
-
-class _SingleMenuState extends State<SingleMenu> {
-  @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    return Column(
-      children: [
-        Container(
-          height: 50,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              SizedBox(width: size.width*0.1),
-              Text(widget.rcpItem.receiptItemName,
-                style: TextStyle(
-                  fontSize: 17,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              SizedBox(width: size.width*0.05),
-              Text("2명",
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w600,
-                  color: basic[2],
-                ),
-              ),
-              SizedBox(width: size.width*0.35),
-              Text(widget.rcpItem.price.toString(),
-                style: TextStyle(
-                  fontSize: 17,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-
-            ],
           ),
         ),
         Container(
