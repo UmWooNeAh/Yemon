7bb4eae4c2f3e330ebfa3fa4a52afe22d8221941
anacnoda77 <sm1108shin@naver.com>
2024-01-28 18:07:45 +0900

    Feat.사진 저장->카카오톡 공유하기 연동
    보내기 양식때문에 결과값이 좋지 못한 느낌 일단은 이정도만 구현해두는 것으로

--- lib/Kakao/kakao_common.dart
+++ lib/Kakao/kakao_common.dart
@@ -1,55 +1,96 @@
 import 'dart:developer';
+import 'package:flutter/cupertino.dart';
 import 'package:kakao_flutter_sdk_share/kakao_flutter_sdk_share.dart';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:path_provider/path_provider.dart';
+
+// final FeedTemplate defaultFeed = FeedTemplate(
+//   content: Content(
+//     title: '딸기 치즈 케익',
+//     description: '#케익 #딸기 #삼평동 #카페 #분위기 #소개팅',
+//     imageUrl: Uri.parse(
+//         'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png'),
+//     link: Link(
+//         webUrl: Uri.parse('https://developers.kakao.com'),
+//         mobileWebUrl: Uri.parse('https://developers.kakao.com')),
+//   ),
+//   itemContent: ItemContent(
+//     profileText: 'Kakao',
+//     profileImageUrl: Uri.parse(
+//         'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png'),
+//     titleImageUrl: Uri.parse(
+//         'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png'),
+//     titleImageText: 'Cheese cake',
+//     titleImageCategory: 'cake',
+//     items: [
+//       ItemInfo(item: 'cake1', itemOp: '1000원'),
+//       ItemInfo(item: 'cake2', itemOp: '2000원'),
+//       ItemInfo(item: 'cake3', itemOp: '3000원'),
+//       ItemInfo(item: 'cake4', itemOp: '4000원'),
+//       ItemInfo(item: 'cake5', itemOp: '5000원')
+//     ],
+//     sum: 'total',
+//     sumOp: '15000원',
+//   ),
+//   social: Social(likeCount: 286, commentCount: 45, sharedCount: 845),
+//   buttons: [
+//     Button(
+//       title: '웹으로 보기',
+//       link: Link(
+//         webUrl: Uri.parse('https: //developers.kakao.com'),
+//         mobileWebUrl: Uri.parse('https: //developers.kakao.com'),
+//       ),
+//     ),
+//     Button(
+//       title: '앱으로보기',
+//       link: Link(
+//         androidExecutionParams: {'key1': 'value1', 'key2': 'value2'},
+//         iosExecutionParams: {'key1': 'value1', 'key2': 'value2'},
+//       ),
+//     ),
+//   ],
+// );
 
 
-final FeedTemplate defaultFeed = FeedTemplate(
-  content: Content(
-    title: '딸기 치즈 케익',
-    description: '#케익 #딸기 #삼평동 #카페 #분위기 #소개팅',
-    imageUrl: Uri.parse(
-        'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png'),
-    link: Link(
-        webUrl: Uri.parse('https://developers.kakao.com'),
-        mobileWebUrl: Uri.parse('https://developers.kakao.com')),
-  ),
-  itemContent: ItemContent(
-    profileText: 'Kakao',
-    profileImageUrl: Uri.parse(
-        'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png'),
-    titleImageUrl: Uri.parse(
-        'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png'),
-    titleImageText: 'Cheese cake',
-    titleImageCategory: 'cake',
-    items: [
-      ItemInfo(item: 'cake1', itemOp: '1000원'),
-      ItemInfo(item: 'cake2', itemOp: '2000원'),
-      ItemInfo(item: 'cake3', itemOp: '3000원'),
-      ItemInfo(item: 'cake4', itemOp: '4000원'),
-      ItemInfo(item: 'cake5', itemOp: '5000원')
-    ],
-    sum: 'total',
-    sumOp: '15000원',
-  ),
-  social: Social(likeCount: 286, commentCount: 45, sharedCount: 845),
-  buttons: [
-    Button(
-      title: '웹으로 보기',
-      link: Link(
-        webUrl: Uri.parse('https: //developers.kakao.com'),
-        mobileWebUrl: Uri.parse('https: //developers.kakao.com'),
+Future<FeedTemplate> makeTemplate(Uint8List uint8list) async {
+
+  Directory tempDir = await getApplicationDocumentsDirectory();
+  String tempPath = '${tempDir.path}/abc.txt';
+  log("경로: ${tempPath}");
+
+  File file = File(tempPath); await file.writeAsBytes(uint8list);
+
+  try {
+    // 카카오 이미지 서버로 업로드
+    ImageUploadResult imageUploadResult =
+        await ShareClient.instance.uploadImage(image: file);
+    print('이미지 업로드 성공'
+        '\n${imageUploadResult.infos.original}');
+
+    final FeedTemplate template = FeedTemplate(
+      content: Content(
+        title: "정산서",
+        imageUrl: Uri.parse(imageUploadResult.infos.original.url),
+        link: Link(
+            mobileWebUrl: Uri.parse('https://developers.kakao.com')),
       ),
-    ),
-    Button(
-      title: '앱으로보기',
-      link: Link(
-        androidExecutionParams: {'key1': 'value1', 'key2': 'value2'},
-        iosExecutionParams: {'key1': 'value1', 'key2': 'value2'},
-      ),
-    ),
-  ],
-);
+    );
 
-void shareMessage() async {
+    return template;
+
+  } catch (error) {
+    print('이미지 업로드 실패 $error');
+  }
+
+  exit(0);
+}
+
+void shareMessage(Uint8List image) async {
+
+  final createdTemplate = await makeTemplate(image);
+
   // 카카오톡 실행 가능 여부 확인
   bool isKakaoTalkSharingAvailable = await ShareClient.instance
       .isKakaoTalkSharingAvailable();
@@ -57,7 +98,7 @@ void shareMessage() async {
   if (isKakaoTalkSharingAvailable) {
     try {
       Uri uri =
-      await ShareClient.instance.shareDefault(template: defaultFeed);
+      await ShareClient.instance.shareDefault(template: createdTemplate);
       await ShareClient.instance.launchKakaoTalk(uri);
       log('카카오톡 공유 완료');
     } catch (error) {
@@ -67,7 +108,7 @@ void shareMessage() async {
   else {
     try {
       Uri shareUrl = await WebSharerClient.instance
-          .makeDefaultUrl(template: defaultFeed);
+          .makeDefaultUrl(template: createdTemplate);
       await launchBrowserTab(shareUrl, popupOpen: true);
     } catch (error) {
       log('카카오톡 공유 실패 $error');
--- lib/View/settlement_check.dart
+++ lib/View/settlement_check.dart
@@ -9,6 +9,7 @@ import 'dart:typed_data';
 import 'dart:ui' as ui;
 import 'package:flutter/rendering.dart' hide BoxShadow, BoxDecoration;
 import 'package:image_gallery_saver/image_gallery_saver.dart';
+import '../Kakao/kakao_common.dart';
 import '../shared_tool.dart';
 import '../theme.dart';
 
@@ -262,7 +263,11 @@ class _SettlementCheckState extends ConsumerState<SettlementCheck> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 InkWell(
-                  onTap: () {},
+                  onTap: () {
+                    captureWidget().then((value) {
+                      shareMessage(value);
+                    });
+                    },
                   child: Container(
                     width: size.width * 0.5 - 15,
                     margin:
