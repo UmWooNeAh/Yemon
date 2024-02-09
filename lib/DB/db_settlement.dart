2ddd6a1f8b5b203f5187997974028c4159efeab6
anacnoda77 <sm1108shin@naver.com>
2024-02-09 22:29:37 +0900

    Refactor. 정산 인스턴스 수정
    Settlement 내 RecordDate를 초 단위까지 저장하도록 수정

--- lib/DB/db_settlement.dart
+++ lib/DB/db_settlement.dart
@@ -8,7 +8,7 @@ class DBSettlement {
 
   Future<int> createStm(Database db, String stmId, String stmName) async {
     return await db!.rawInsert('INSERT INTO Settlement(settlementId, settlementName, recordDate) VALUES(?, ?, ?)'
-        , [stmId, stmName, DateFormat('yyyy-MM-dd HH:mm').format(DateTime.now())]);
+        , [stmId, stmName, DateFormat('yyyy-MM-dd HH:mm:ss').format(DateTime.now())]);
   }
 
   Future<List<Map>> readStm(Database db, String stmId) async {
@@ -17,7 +17,7 @@ class DBSettlement {
 
   Future<int> updateStm(Database db, String newstmName, String stmId) async {
     return await db!.rawUpdate('UPDATE Settlement SET settlementName = ?, recordDate = ? WHERE settlementId = ?'
-        , [newstmName, DateFormat('yyyy-MM-dd HH:mm').format(DateTime.now()), stmId]);
+        , [newstmName, DateFormat('yyyy-MM-dd HH:mm:ss').format(DateTime.now()), stmId]);
   }
 
   Future<int> deleteStm(Database db, String stmId) async {
